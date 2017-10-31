2eef3321d79c3872963df7c6feb50f6fdb38d61a
Sam Zhang <samanthazhang9@gmail.com>
2017-10-31 15:58:36 -0400

    order model working

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@
 const User = require('./user')
 const Product = require ('./product')
 const Address = require('./address');
+const Order = require('./order');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +14,10 @@ const Address = require('./address');
  User.hasMany(Address);
  Address.belongsTo(User);
 
+ Order.belongsTo(User);
+ Order.belongsTo(Address);
+ User.hasMany(Order);
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -22,5 +27,6 @@ const Address = require('./address');
 module.exports = {
   User,
   Product,
-  Address
+  Address,
+  Order
 }
