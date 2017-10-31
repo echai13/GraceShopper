4d89c298174d3e581a8d2522ed2e975e368d9b9d
Caryn McCarthy <cmccarthy15@gmail.com>
2017-10-31 12:23:03 -0400

    created all products GET route and test

--- server/api/products.js
+++ server/api/products.js
@@ -1,14 +1,9 @@
 const router = require('express').Router()
-const {Product} = require('../db/product')
+const {Product} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Product.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-    attributes: ['id', 'email']
-  })
-    .then(users => res.json(users))
+  Product.findAll()
+    .then(products => res.json(products))
     .catch(next)
 })
