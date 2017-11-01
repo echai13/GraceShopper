1bb6c106204b5111455760f28f184c127fbb4658
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-01 11:31:34 -0400

    updated serverside products routes and test to ensure eager loading of catgories

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const Product = db.model('product')
+const Category = db.model('category')
 
 describe('Product routes', () => {
   beforeEach(() => {
@@ -13,21 +14,25 @@ describe('Product routes', () => {
 
   describe('/api/products/', () => {
 
-    beforeEach(() => {
-      return Product.create({
+    beforeEach( async () => {
+      const categoryOne = await Category.create({ name: 'plant-type' });
+      const categoryTwo = await Category.create({ name: 'water-type' });
+      const productTest = await Product.create({
         name: 'Cody',
         price: '1.45',
         description: 'cody is a good puppy'
       })
+      await productTest.setCategories([categoryOne,categoryTwo]);
     })
 
-    it('GET /api/products', () => {
+    it('GET /api/products including categories', () => {
       return request(app)
         .get('/api/products')
         .expect(200)
         .then(res => {
           expect(res.body).to.be.an('array')
           expect(res.body[0].name).to.be.equal('Cody')
+          expect(res.body[0].categories).to.have.lengthOf(2);
         })
     })
   }) // end describe('/api/products')
