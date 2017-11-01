c61513f866b9e23df66bf7bb0e1ea2af8ee074a5
echai13 <cher13@brandeis.edu>
2017-11-01 11:38:17 -0400

    navbar-component

--- client/components/main.js
+++ client/components/main.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 import {logout} from '../store'
+import NavBar from './NavBar'
 
 /**
  * COMPONENT
@@ -11,28 +12,15 @@ import {logout} from '../store'
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+  const {children} = props
 
   return (
     <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
-      {children}
+      <NavBar />
+      <div className="container">
+        <hr />
+        {children}
+      </div>
     </div>
   )
 }
@@ -41,17 +29,11 @@ const Main = (props) => {
  * CONTAINER
  */
 const mapState = (state) => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
+  return null
 }
 
 const mapDispatch = (dispatch) => {
-  return {
-    handleClick () {
-      dispatch(logout())
-    }
-  }
+  return null
 }
 
 // The `withRouter` wrapper makes sure that updates are not blocked
@@ -61,8 +43,3 @@ export default withRouter(connect(mapState, mapDispatch)(Main))
 /**
  * PROP TYPES
  */
-Main.propTypes = {
-  children: PropTypes.object,
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
