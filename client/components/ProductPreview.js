198fc49b832602585fdadd374caa75a5f06ff930
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-02 08:06:59 -0400

    completed product preview component. should not update products component until merge with branch 59

--- client/components/ProductPreview.js
+++ client/components/ProductPreview.js
@@ -1,4 +1,23 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import history from '../history'
+
+const ProductPreview = (props) => {
+  const product = props.product;
+
+  return (
+    <div key={product.id}>
+      <Link to={`/products/${product.id}`}>
+        <h3>{product.name}</h3>
+        <img src={product.image} />
+      </Link>
+      <h3>{product.price}</h3>
+      <p>{product.description}</p>
+      <h3>{product.stock} </h3>
+      <h3>{product.isAvailable} </h3>
+      <h3>{product.category}</h3>
+      <h3>{product.reviews}</h3>
+    </div>
+  )
+}
+
+export default ProductPreview;
