6986483fc72e39ebb4259c43f7d330b2931c089e
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-03 12:40:32 -0400

    can update numer from inproduct page but messes up in cart

--- server/api/order.js
+++ server/api/order.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const withCart = require('./withCart');
-const { OrderItem, Order } = require('../db/models')
+const { OrderItem, Order, Product } = require('../db/models')
 
 module.exports = router
 
@@ -11,16 +11,89 @@ router.get('/', (req, res, next) => {
     res.json(req.cart)
 });
 
-router.put('/', async (req, res, next) => {
-  console.log('inside of router PUT /api/order with req.cart: ', req.cart, ' and req.body: ', req.body);
+// put /incart
+/*
+  find the orderItem and update it (swap out quantity values)
+*/
+
+router.put('/incart', async (req, res, next) => {
+  const { quantity, productId } = req.body.productInfo;
+
+  const orderItem = await OrderItem.findOne({
+    where: { productId: productId, orderId: req.cart.id }
+  })
+
+  await orderItem.update({ quantity: quantity })
+  req.cart = await Order.findById(req.cart.id)
+  res.json(req.cart);
+})
+
+router.put('/inproduct', async (req, res, next) => {
   const { currentPrice, quantity, productId } = req.body.productInfo;
 
+  const orderItem = await OrderItem.findOne({
+    where: { productId: productId, orderId: req.cart.id },
+    include: [{ model: Product }]
+  })
+
+  if (!orderItem) {
+    req.method = 'POST';
+    res.redirect('/api/order/neworderitem');
+  }
+  // get the previous quantity, add the new quantity to it... if that's higher than the stock, update with the stock and if its not, update with the new quantity
+  let oldQuan, newQuan, stock;
+  stock = orderItem.product.stock;
+  oldQuan = orderItem.quantity;
+  newQuan = (quantity + oldQuan) > stock ?
+              stock
+              : (quantity + oldQuan);
+
+  await orderItem.update({ quantity: newQuan })
+
+  req.cart = await Order.findById(req.cart.id)
+  res.json(req.cart);
+
+})
+
+router.post('/neworderitem', (req, res, next) => {
+  const { currentPrice, quantity, productId } = req.body.productInfo;
+
+  OrderItem.create({ currentPrice, quantity, productId, orderId: req.cart.id })
+    .then(async () => {
+      req.cart = await Order.findById(req.cart.id)
+      res.json(req.cart);
+    })
+    .catch(next);
+
+})
+// put /inproduct
+/*
+  see if there is an order item... if there is add the new quantity up to the item's stock
+  if there isn't that order item yet, create it
+*/
+
+router.put('/', async (req, res, next) => {
+  const { currentPrice, quantity, productId } = req.body.productInfo;
+  console.log('inside of put request', productId, req.cart.id);
+
 	const orderItem = await OrderItem.findOne( {
-  	where: {productId: productId , orderId: req.cart.id} })
+    where: {productId: productId, orderId: req.cart.id},
+    include: [{model: Product }] })
+
+  console.log('order item is: ', orderItem);
+  console.log('item stock: ', orderItem.product.stock)
   	if (orderItem) {
-  		const newQuantity = await orderItem.update({ quantity: quantity})
-  		req.cart = await Order.findById(req.cart.id)
- 		res.json(req.cart);
+      let oldQuan, newQuan, stock;
+      stock = Number(orderItem.product.stock);
+      oldQuan = Number(orderItem.quantity);
+      newQuan = Number(quantity) + oldQuan > stock ?
+        stock
+        : Number(quantity) + oldQuan;
+
+      await orderItem.update({ quantity: newQuan })
+
+      req.cart = await Order.findById(req.cart.id)
+      res.json(req.cart);
   	} else {
   		OrderItem.create({currentPrice, quantity, productId, orderId: req.cart.id })
 		    .then( async () => {
