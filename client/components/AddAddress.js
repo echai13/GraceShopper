b29d399551c5753aa98b82a6724d1034d9dfba6b
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-06 18:06:35 -0500

    add users component renders and adds a new address for the correct user

--- client/components/AddAddress.js
+++ client/components/AddAddress.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { setAddressThunk } from '../store'
+import { withRouter } from 'react-router'
+import { addAddress } from '../store'
 
 /**
  * COMPONENT
@@ -20,67 +20,71 @@ export class AddAddress extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    this.props.fetchAddresses(this.props.id);
-  }
-
   handleSubmit(evt) {
     evt.preventDefault();
-
+    const addressInfo = this.state;
+    addressInfo.userId = this.props.id;
+    this.props.addAddress(addressInfo)
   }
 
   handleChange(evt) {
-    const key = evt.target.name;
-    const value = evt.target.value;
-    this.setState({key: value})
+    this.setState({ [evt.target.name]: evt.target.value})
   }
 
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+        Add a New Address
         <label>
           Street 1:
-          <input type="text"
+          <input
+          type="text"
             name="street1"
             value={this.state.street1}
             onChange={this.handleChange} />
         </label>
         <label>
           Street 2:
-          <input type="text"
+          <input
+          type="text"
             name="street2"
             value={this.state.street2}
             onChange={this.handleChange} />
         </label>
         <label>
           City:
-          <input type="text"
+          <input
+          type="text"
             name="city"
             value={this.state.city}
             onChange={this.handleChange} />
         </label>
         <label>
           State:
-          <input type="text"
+          <input
+            type="text"
             name="state"
             value={this.state.state}
             onChange={this.handleChange} />
         </label>
         <label>
           Zipcode:
-          <input type="text"
+          <input
+            type="text"
             name="zipcode"
             value={this.state.zipcode /* zipcode will need to be a number */ }
-            onChange={this.handleChange} /> /
+            onChange={this.handleChange} />
         </label>
         <label>
           Country:
-          <input type="text"
+          <input
+            type="text"
             name="country"
             value={this.state.country}
             onChange={this.handleChange} />
         </label>
+        <input type="submit" value="Submit" />
       </form>
     )
   }
@@ -96,8 +100,13 @@ const mapState = (state) => {
 }
 
 const mapDispatch = (dispatch) => {
-  return null;
+  return {
+    addAddress(addressInfo){
+      dispatch(addAddress(addressInfo));
+    }
   }
 }
 
-export default connect(mapState, mapDispatch)(AddAddress)
+// could probably take off withRouter... originally put there in case
+// we would have it take them to a new page and then use goBack to return to the prev page
+export default withRouter(connect(mapState, mapDispatch)(AddAddress))
