d3f07152e82bb0a240d7f84a65b4439bbc857e9b
Caryn McCarthy <cmccarthy15@gmail.com>
2017-11-06 18:36:44 -0500

    userdetails also renders AddAddresses and it works

--- client/components/user-details.js
+++ client/components/user-details.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import { setAddressThunk } from '../store'
+import { AddAddress } from './index'
 
 /**
  * COMPONENT
  */
 export class UserDetails extends Component {
+  constructor(){
+    super();
+    this.state = {
+      addAddress: false
+    }
+    this.hideAdd = this.hideAdd.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchAddresses(this.props.id);
   }
 
+  hideAdd() {
+    this.setState({ addAddress: false })
+  }
+
   render() {
     return (
       <div>
-        { 
+        {
           this.props.addresses.map(address => {
-            return address ? <div key={address.id}><p>{address.street1}</p>
-            <p>{address.street2}</p>
-            <p>{address.city}, {address.state} {address.zipcode}</p>
-            <p>{address.country}</p> </div> : ''
+            return address ?
+            <div key={address.id}>
+              <h3>Address</h3>
+              <p>{address.street1}</p>
+              <p>{address.street2}</p>
+              <p>{address.city}, {address.state} {address.zipcode}</p>
+              <p>{address.country}</p>
+            </div> : ''
           })
         }
+        <button
+          onClick={() => {
+            this.setState({ addAddress: !this.state.addAddress })
+          }}>
+          Add Address
+          </button>
+        {this.state.addAddress && <AddAddress hide={this.hideAdd} />}
       </div>
     )
   }
